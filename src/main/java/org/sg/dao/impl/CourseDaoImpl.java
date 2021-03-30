7968b2e788ba492f3e1605c0408a919332735b29
Ermal <ermal.aliraj@gmail.com>
2021-03-30 14:57:56 +0200

    Spring minimal config

--- src/main/java/org/sg/dao/impl/CourseDaoImpl.java
+++ src/main/java/org/sg/dao/impl/CourseDaoImpl.java
@@ -1,55 +1,42 @@
 package org.sg.dao.impl;
 
 import org.sg.dao.CourseDao;
-import org.sg.dao.CustomHibernateDaoSupport;
-import org.sg.entities.AddressEntity;
+import org.sg.dao.GenericDao;
 import org.sg.entities.CourseEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CourseDaoImpl extends CustomHibernateDaoSupport implements CourseDao{
+public class CourseDaoImpl extends GenericDao implements CourseDao{
 
 	@Override
 	public CourseEntity insert(CourseEntity courseEntity) {
-//		entityManager.getTransaction().begin();
-//		entityManager.persist(courseEntity);
-//		entityManager.getTransaction().commit();
-//		return courseEntity;
-		getHibernateTemplate().save(courseEntity);
+		entityManager.getTransaction().begin();
+		entityManager.persist(courseEntity);
+		entityManager.getTransaction().commit();
 		return courseEntity;
 	}
 
 	@Override
 	public CourseEntity get(Integer id) {
-//		CourseEntity courseEntity = entityManager.find(CourseEntity.class, id);
-//		return courseEntity;
-		CourseEntity courseEntity = getHibernateTemplate().get(CourseEntity.class, id);
+		CourseEntity courseEntity = entityManager.find(CourseEntity.class, id);
 		return courseEntity;
 	}
 
 	@Override
 	public CourseEntity update(CourseEntity courseEntity) {
-//		entityManager.getTransaction().begin();
-//		entityManager.persist(courseEntity);
-//		entityManager.getTransaction().commit();
-//		return courseEntity;
-		getHibernateTemplate().update(courseEntity);
+		entityManager.getTransaction().begin();
+		entityManager.persist(courseEntity);
+		entityManager.getTransaction().commit();
 		return courseEntity;
 	}
 
 	@Override
 	public boolean delete(Integer id) {
-//		CourseEntity courseEntity = entityManager.find(CourseEntity.class, id);
-//		if (courseEntity != null) {
-//			entityManager.getTransaction().begin();
-//			entityManager.remove(courseEntity);
-//			entityManager.getTransaction().commit();
-//			return true;
-//		}
-//		return false;
-		CourseEntity courseEntity = getHibernateTemplate().get(CourseEntity.class, id);
-		if(courseEntity != null) {
-			getHibernateTemplate().delete(courseEntity);
+		CourseEntity courseEntity = entityManager.find(CourseEntity.class, id);
+		if (courseEntity != null) {
+			entityManager.getTransaction().begin();
+			entityManager.remove(courseEntity);
+			entityManager.getTransaction().commit();
 			return true;
 		}
 		return false;
